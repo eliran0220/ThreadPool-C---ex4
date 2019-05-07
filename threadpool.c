9f534b8065c8a3eb489301e2f4b9b9813b97173b
eliran0220 <eliran0220@gmail.com>
2019-05-07 17:35:54 +0300

    Add files via upload

--- threadpool.c
+++ threadpool.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include "threadPool.h"
 
-#define ERROR_ALLOCATION "Error in system call\n"
+#define ERROR_MESSAGE "Error in system call\n"
 #define ERROR_SIZE 21
 #define STDERR 2
 #define EXIT_FAIL -1
 #define SUCCESS 0
 #define YES 1
+#define NO 0
+#define TWIECE_DESTROYED_ERROR "Error - destroyed pool twice from same call\n"
+#define TWICE_ERROR_SIZE 44
 
-#define THREADS_COUNT 4
-#define TASKS_PER_THREAD 30
-#define TASKS_PRI_THREAD 10
-#define TP_WORKER_THREADS 3
+pthread_mutex_t lockThePool;
 
-#define DEBUG 1 // Change to 1 for debug info
+void hello (void* a)
+{
+    printf("hello\n");
+}
 
-pthread_mutex_t TasksDoneLock;
-pthread_mutex_t TasksInsertedLock;
 
-volatile int tasksDoneCount;
-volatile int tasksInsertedCount;
+void test_thread_pool_sanity()
+{
+    int i;
 
+    ThreadPool* tp = tpCreate(5);
+
+    for(i=0; i<5; ++i)
+    {
+        tpInsertTask(tp,hello,NULL);
+    }
+
+    tpDestroy(tp,1);
+}
+
+
+
+void errorTwiceDestroyed ();
 
 void error();
 
@@ -46,14 +61,15 @@ ThreadPool *tpCreate(int numOfThreads) {
     if (threadPool->osQueue == NULL) {
         error();
     }
-    if (pthread_mutex_init(&(threadPool->lockQueue), NULL) != 0
-        || pthread_cond_init(&(threadPool->notify), NULL) != 0) {
+    if (pthread_mutex_init(&(threadPool->lockQueue), NULL) != 0 ||
+    pthread_cond_init(&(threadPool->notify), NULL) != 0 ||
+    pthread_mutex_init(&(lockThePool), NULL) != 0) {
         error();
     }
-    threadPool->isStopped = 0;
+    threadPool->isStopped = NO;
     threadPool->executeTasks = executeTasks;
     threadPool->numOfThreads = numOfThreads;
-    threadPool->destory = NoDestroy;
+    threadPool->destroy = NoDestroy;
     int i;
     for (i = 0; i < numOfThreads; i++) {
         pthread_create(&threadPool->threads[i], NULL, runTasks, threadPool);
@@ -82,29 +98,37 @@ int tpInsertTask(ThreadPool *threadPool, void (*computeFunc)(void *), void *para
 }
 
 void tpDestroy(ThreadPool *threadPool, int shouldWaitForTasks) {
+    pthread_mutex_lock(&lockThePool);
     if (threadPool->isStopped) {
-        return;
+        pthread_mutex_destroy(&lockThePool);
+        errorTwiceDestroyed();
     }
-    if (shouldWaitForTasks != 0) {
-        threadPool->destory = waitAll;
+    if (shouldWaitForTasks != NO) {
+        threadPool->destroy = waitAll;
         freeThreadPool(threadPool);
     } else {
-        threadPool->destory = waitOnlyRunning;
+        threadPool->destroy = waitOnlyRunning;
         freeThreadPool(threadPool);
     }
-
+    pthread_mutex_unlock(&lockThePool);
 }
 
 void error() {
-    write(STDERR, ERROR_ALLOCATION, ERROR_SIZE);
+    write(STDERR, ERROR_MESSAGE, ERROR_SIZE);
     exit(EXIT_FAIL);
 }
 
+void errorTwiceDestroyed () {
+    write(STDERR,TWIECE_DESTROYED_ERROR,TWICE_ERROR_SIZE);
+    exit(EXIT_FAIL);
+}
+
+
 void executeTasks(void *args) {
     ThreadPool *threadPool = (ThreadPool *) args;
-    while (threadPool->isStopped == 0) {
+    while (threadPool->isStopped == NO) {
         pthread_mutex_lock(&threadPool->lockQueue);
-        if (threadPool->destory == NoDestroy) {
+        if (threadPool->destroy == NoDestroy) {
             if (osIsQueueEmpty(threadPool->osQueue)) {
                 // handle busy waiting for task to be inserted
                 pthread_cond_wait(&(threadPool->notify), &(threadPool->lockQueue));
@@ -119,9 +143,9 @@ void executeTasks(void *args) {
                 break;
             }
 
-            if (threadPool->destory == waitAll) {
+            if (threadPool->destroy == waitAll) {
                 completeTask(threadPool);
-            } else if (threadPool->destory == waitOnlyRunning) {
+            } else if (threadPool->destroy == waitOnlyRunning) {
                 pthread_mutex_unlock(&threadPool->lockQueue);
                 break;
             }
@@ -145,7 +169,6 @@ void completeTask(ThreadPool *th) {
 }
 
 void freeThreadPool(ThreadPool *threadPool) {
-
     int i;
     pthread_mutex_lock(&(threadPool->lockQueue));
     pthread_cond_broadcast(&(threadPool->notify));
@@ -164,6 +187,7 @@ void freeThreadPool(ThreadPool *threadPool) {
     //destroy mutexes and cond
     pthread_mutex_destroy(&(threadPool->lockQueue));
     pthread_cond_destroy(&(threadPool->notify));
+
     //destroy threads array
     free(threadPool->threads);
     //destroy thread pool
@@ -171,143 +195,8 @@ void freeThreadPool(ThreadPool *threadPool) {
 }
 
 
-void incTaskAdded() {
-    pthread_mutex_lock(&TasksInsertedLock);
-    tasksInsertedCount++;
-    pthread_mutex_unlock(&TasksInsertedLock);
-}
-
-void incTaskDone() {
-    pthread_mutex_lock(&TasksDoneLock);
-    tasksDoneCount++;
-    pthread_mutex_unlock(&TasksDoneLock);
-}
-
-int getCurrentThread() {
-    return ((long) pthread_self() % 1000);
-}
-
-
-void task1(void *_) {
-    int r;
-    r = (rand() % 100) + 20;
-    if (DEBUG) printf("TASK1 thread %d. sleeping %dms\n", (getCurrentThread()), r);
-    usleep(r);
-    incTaskDone();
-}
-
-void task2(void *_) {
-    if (DEBUG) printf("TASK2 thread %d\n", getCurrentThread());
-    incTaskDone();
-}
-
-void *poolDestoyer(void *arg) {
-    ThreadPool *pool = (ThreadPool *) arg;
-    tpDestroy(pool, 1);
-    return NULL;
-}
-
-void *tasksAdder(void *arg) {
-    ThreadPool *pool = (ThreadPool *) arg;
-    int i;
-
-    for (i = 0; i < TASKS_PER_THREAD; ++i) {
-        if (!tpInsertTask(pool, task1, NULL)) {
-            incTaskAdded();
-        }
-    }
-
-    return NULL;
-}
-
-int shouldWaitForTasksTest(int shouldWait) {
-    pthread_t t1[THREADS_COUNT];
-    int i, j, result;
-    ThreadPool *tp = tpCreate(TP_WORKER_THREADS);
-    for (j = 0; j < THREADS_COUNT; j++) {
-        pthread_create(&t1[j], NULL, tasksAdder, tp);
-    }
-
-    for (i = 0; i < TASKS_PRI_THREAD; i++) {
-        if (!tpInsertTask(tp, task2, NULL)) {
-            incTaskAdded();
-        }
-    }
-
-    if (DEBUG) printf("-->tp will be destroyed!<--\n");
-    tpDestroy(tp, shouldWait);
-    if (DEBUG) printf("-->tp destroyed!<--\n");
-
-    if (DEBUG) printf("waiting for other threads to end..\n");
-    for (j = 0; j < THREADS_COUNT; j++) {
-        pthread_join(t1[j], NULL);
-    }
-    pthread_mutex_lock(&TasksInsertedLock);
-    pthread_mutex_lock(&TasksDoneLock);
-    if (DEBUG) printf("\nSUMMRAY:\nTasks inserted:%d\nTasks done:%d\n", tasksInsertedCount, tasksDoneCount);
-    if (DEBUG) printf("Graceful? %d\n", shouldWait);
-    if ((shouldWait && tasksInsertedCount == tasksDoneCount) ||
-        (!shouldWait && tasksInsertedCount != tasksDoneCount)) {
-        result = 0;
-    } else {
-        result = 1;
-    }
-
-    tasksDoneCount = 0;
-    tasksInsertedCount = 0;
-    pthread_mutex_unlock(&TasksInsertedLock);
-    pthread_mutex_unlock(&TasksDoneLock);
-
-    return result;
-}
-
-int insertAfterDestroyTest() {
-    ThreadPool *tp = tpCreate(TP_WORKER_THREADS);
-    int i;
-    usleep(50);
-    for (i = 0; i < TASKS_PRI_THREAD; i++) {
-        tpInsertTask(tp, task1, NULL);
-    }
-    tpDestroy(tp, 1);
-    return !tpInsertTask(tp, task1, NULL);
-}
-
-int doubleDestroy() {
-    pthread_t t1;
-    ThreadPool *tp = tpCreate(TP_WORKER_THREADS);
-    int i;
-    for (i = 0; i < TASKS_PRI_THREAD; i++) {
-        tpInsertTask(tp, task1, NULL);
-    }
-    printf("Going to destroy pool from 2 different threads...\n");
-    pthread_create(&t1, NULL, poolDestoyer, tp);
-    tpDestroy(tp, 1);
-    pthread_join(t1, NULL);
-    printf("Done, did anything break?\n");
-    return 0;
-}
-
 int main() {
-    srand(time(NULL));
-    pthread_mutex_init(&TasksDoneLock, NULL);
-    pthread_mutex_init(&TasksInsertedLock, NULL);
-    tasksDoneCount = 0;
-    tasksInsertedCount = 0;
-    int i;
-    printf("---Tester Running---\n");
-
-    for (i = 0; i < 10; i++) {
-        if (insertAfterDestroyTest())
-            printf("Could insert task after tp destroyed!\n");
-        if (shouldWaitForTasksTest(0))
-            printf("Failed on shouldWaitForTasks = 0, tasks created = tasks done. This should rarely happen..\n");
-        if (shouldWaitForTasksTest(1))
-            printf("Failed on destroy with shouldWaitForTasks = 1. tasks created != tasks done\n");
-    }
-
-    doubleDestroy();
-
-    printf("---Tester Done---\n");
+    test_thread_pool_sanity();
     return 0;
 }
 
